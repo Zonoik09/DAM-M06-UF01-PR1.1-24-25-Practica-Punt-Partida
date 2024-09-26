49c495ebf112aa2aed6f26f212f0be13230121e6
Jonathan Rueda <jonaneiro09@gmail.com>
2024-09-26 15:41:26 +0200

    Activity1 - 2

--- src/main/java/com/project/PR110ReadFile.java
+++ src/main/java/com/project/PR110ReadFile.java
@@ -1,5 +1,12 @@
 package com.project;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PR110ReadFile {
 
     public static void main(String[] args) {
@@ -9,6 +16,41 @@ public class PR110ReadFile {
 
     // Funció que llegeix el fitxer i mostra les línies amb numeració
     public static void llegirIMostrarFitxer(String camiFitxer) {
+        int counter = 1;
+        File fitxer = new File(camiFitxer);
+        try (Scanner scanner = new Scanner(fitxer)) {
+            while (scanner.hasNextLine()) {
+                String linia = scanner.nextLine();
+                System.out.println(counter+": "+linia);
+                counter ++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo de la siguiente ruta no existe o no se ha encontrado: " + camiFitxer);
+            crearArchivo(camiFitxer);
+        }
+    }
 
+    // Si no existe ese archivo abra la posibilidad de crear un archivo con el nombre que ha puesto.
+    public static void crearArchivo(String archivo) {
+        BufferedWriter bw;
+        System.out.println("Quieres crear el archivo? (S/N)");
+        Scanner scanner0 = new Scanner(System.in);
+        String respuesta = scanner0.nextLine().toUpperCase();
+        if (respuesta.equals("S")) {
+            try {
+                bw = new BufferedWriter(new FileWriter(archivo));
+                System.out.println("Creando el archivo...");
+                bw.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println("Cerrando programa...");
+        }
+            
+         
     }
 }
+ 
--- src/main/java/com/project/PR111Files.java
+++ src/main/java/com/project/PR111Files.java
@@ -1,5 +1,12 @@
 package com.project;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
 public class PR111Files {
 
     public static void main(String[] args) {
@@ -8,5 +15,33 @@ public class PR111Files {
     }
 
     public static void gestionarArxius(String camiFitxer) {
+        // Convierto el String en un "fichero"/ruta.
+        File carpeta = new File(camiFitxer);
+        // Compruebo si existe y si no existe creo la carpeta y luego con el for creo los archivos con el standar UTF 8.
+        if (!(carpeta.exists())) {
+            carpeta.mkdir();
+            for (int i = 1; i < 3; i++) {
+                try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(camiFitxer+"/file" + i + ".txt"), StandardCharsets.UTF_8))) {
+                    System.out.println("Creando el archivo: file" + i + ".txt");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } else {
+            // Tengo que comprobar si existe los archivos para crearlos o no.
+            File file = new File(camiFitxer+"\\file1.txt");
+            File file1 = new File(camiFitxer+"\\file2.txt");
+            if (!(file.exists() && file1.exists())) {
+                for (int i = 1; i < 3; i++) {
+                    try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(camiFitxer + "/file" + i + ".txt"), StandardCharsets.UTF_8))) {
+                        System.out.println("Creando el archivo: file" + i + ".txt");
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+        
+        
     }
 }
