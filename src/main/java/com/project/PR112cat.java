df57a4cfcc4e72f590159fdc26c025c2d8330157
Jonathan Rueda <jonaneiro09@gmail.com>
2024-09-27 15:18:04 +0200

    empezar ejercicio 2

--- src/main/java/com/project/PR112cat.java
+++ src/main/java/com/project/PR112cat.java
@@ -1,20 +1,45 @@
 package com.project;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class PR112cat {
 
     public static void main(String[] args) {
         // Comprovar que s'ha proporcionat una ruta com a paràmetre
-        if (args.length == 0) {
-            System.out.println("No s'ha proporcionat cap ruta d'arxiu.");
-            return;
-        }
+//        if (args.length == 0) {
+//            System.out.println("No s'ha proporcionat cap ruta d'arxiu.");
+//            return;
+//        }
 
         // Obtenir la ruta del fitxer des dels paràmetres
-        String rutaArxiu = args[0];
+        String rutaArxiu = System.getProperty("user.dir") + "/data/PR112/arxhivo.txt";
         mostrarContingutArxiu(rutaArxiu);
     }
 
     // Funció per mostrar el contingut de l'arxiu o el missatge d'error corresponent
     public static void mostrarContingutArxiu(String rutaArxiu) {
+        File file = new File(rutaArxiu);
+        try {
+            if (!file.isDirectory()) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
+                    String linia;
+                    while ((linia = br.readLine()) != null) {
+                        System.out.println(linia);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else{
+                System.out.println("El path no correspon a un arxiu, sinó a una carpeta.");
+            }
+
+        } catch (Exception e) {
+            String text = ("El fitxer no existeix o no és accessible.");
+            throw new RuntimeException(text);
+        }
+
+
+
     }
 }
